1611d074674ea3c11b3b94793e3a80dda6a977eb
Anselm Peter <anselm.peter@web.de>
2024-08-02 15:33:30 +0200

    Added COM1 stby, new subtexts for display

--- code.js
+++ code.js
@@ -1,15 +1,18 @@
 let law_result = null;
 const prefixes = ['setx'];
+const [whole, decimal] = [0, 1];
 const values = {
-    'hdg': ['K:HEADING_BUG_SET', 'degrees'],
-    'alt': ['A:AUTOPILOT ALTITUDE LOCK VAR', 'ft']
+    'hdg': ['K:HEADING_BUG_SET', 'degrees', 1],
+    'alt': ['A:AUTOPILOT ALTITUDE LOCK VAR', 'ft', 1],
+    'com1': ['K:COM_STBY_RADIO_SET_HZ', 'MHz', 1000000]
 };
 
-search(prefixes, (query, callback) => {    
+search(prefixes, (query, callback) => {
+    keys = Object.keys(values).join('|');
     law_result = {
         uid: 'law_result_uid',
-        label: 'SETX &lt;HDG|ALT&gt; &lt;new value&gt;',
-        subtext: '',
+        label: 'SETX &lt;field&gt; &lt;new value&gt;',
+        subtext: keys,
         // is_note: true,
         execute: null
     };
@@ -28,8 +31,9 @@ search(prefixes, (query, callback) => {
         return;
     }
 
+    let value = values[data[1]];
     if (data.length <= 3 && (isNaN(data[2]) || data[2] == '')) {
-        law_result.label = 'SETX HDG &lt;new value&gt;'
+        law_result.label = 'SETX ' + data[1] + ' &lt;new value&gt; ' + value[1];
         law_result.subtext = 'Invalid new value';
         callback([law_result]);
         return;
@@ -39,12 +43,12 @@ search(prefixes, (query, callback) => {
 
     law_result = {
         uid: 'law_result_uid',
-        label: 'SETX ' + data[1] + ' ' + newValue,
+        label: 'SETX ' + data[1] + ' ' + newValue + ' ' + value[1],
         subtext: '',
         execute: () => {
             // console.log(newValue);
             let value = values[data[1]];
-            this.$api.variables.set(value[0], value[1], newValue);
+            this.$api.variables.set(value[0], value[1], newValue * value[2]);
             return true;
         }
     };
