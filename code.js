b7594301f1117242693ff4622c7f1c9050f890bb
Anselm Peter <anselm.peter@web.de>
2024-08-02 19:05:26 +0200

    Changed variable names. Changed signals to structs.

--- code.js
+++ code.js
@@ -1,19 +1,19 @@
 let setx_result = null;
 const prefixes = ['setx'];
-const values = {
-    'alt': ['A:AUTOPILOT ALTITUDE LOCK VAR', 'ft', 1, 'Autopilot altitude in ft'],
-    'com1': ['K:COM_STBY_RADIO_SET_HZ', 'MHz', 1000000, 'COM1 standby frequency'],
-    'com1a': ['K:COM_RADIO_SET_HZ', 'MHz', 1000000, 'COM1 active frequency'],
-    'hdg': ['K:HEADING_BUG_SET', 'degrees', 1, 'Heading bug in degrees'],
-    'nav1': ['K:NAV1_STBY_SET_HZ', 'MHz', 1000000, 'NAV1 standby frequency'],
-    'nav1a': ['K:NAV1_RADIO_SET_HZ', 'MHz', 1000000, 'NAV2 standby frequency'],
-    'qnhi': ['K:KOHLSMAN_SET', 'in/hg', 33.8639 * 16, 'Barometric pressure in inch of mercury'],
-    'qnhm': ['K:KOHLSMAN_SET', 'millibar', 16, 'Barometric pressure in millibar'],
-    'vor1': ['K:VOR1_SET', 'degrees', 1, 'VOR1 course in degrees'],
+const variables = {
+    'alt': {name: 'A:AUTOPILOT ALTITUDE LOCK VAR', unit: 'ft', factor: 1, note: 'Autopilot altitude in ft'},
+    'com1': {name: 'K:COM_STBY_RADIO_SET_HZ', unit: 'MHz', factor: 1000000, note: 'COM1 standby frequency'},
+    'com1a': {name: 'K:COM_RADIO_SET_HZ', unit: 'MHz', factor: 1000000, note: 'COM1 active frequency'},
+    'hdg': {name: 'K:HEADING_BUG_SET', unit: 'degrees', factor: 1, note: 'Heading bug in degrees'},
+    'nav1': {name: 'K:NAV1_STBY_SET_HZ', unit: 'MHz', factor: 1000000, note: 'NAV1 standby frequency'},
+    'nav1a': {name: 'K:NAV1_RADIO_SET_HZ', unit: 'MHz', factor: 1000000, note: 'NAV2 standby frequency'},
+    'qnhi': {name: 'K:KOHLSMAN_SET', unit: 'in/hg', factor: 33.8639 * 16, note: 'Barometric pressure in inch of mercury'},
+    'qnhm': {name: 'K:KOHLSMAN_SET', unit: 'millibar', factor: 16, note: 'Barometric pressure in millibar'},
+    'vor1': {name: 'K:VOR1_SET', unit: 'degrees', factor: 1, note: 'VOR1 course in degrees'},
 };
 
 search(prefixes, (query, callback) => {
-    keys = Object.keys(values).join('|'); // format keys to list
+    keys = Object.keys(variables).join('|'); // format keys to list
     setx_result = {
         uid: 'setx_result_uid',
         label: 'SETX &lt;field&gt; &lt;new value&gt;',
@@ -26,15 +26,15 @@ search(prefixes, (query, callback) => {
     
     // test if query has 
     let data = query.toLowerCase().split(' ');
-    if (data.length == 1 || (data[1] != '' && !(data[1] in values)) ) {
+    if (data.length == 1 || !data[1] || (data[1] != '' && !(data[1] in variables)) ) {
         callback([setx_result]);
         return;
     }
 
-    let value = values[data[1]];
+    let variable = variables[data[1]];
     if ((isNaN(data[2]) || data[2] == '')) {
-        setx_result.label = 'SETX ' + data[1] + ' &lt;new value&gt; ' + value[1];
-        setx_result.subtext = '<p>' + value[3] + '<br>Invaild value!</p>';
+        setx_result.label = 'SETX ' + data[1] + ' &lt;new value&gt; ' + variable.unit;
+        setx_result.subtext = '<p>' + variable.note + '<br>Invaild value!</p>';
         is_note = false;
         callback([setx_result]);
         return;
@@ -43,11 +43,11 @@ search(prefixes, (query, callback) => {
     let newValue = parseFloat(data[2]);
     setx_result = {
         uid: 'setx_result_uid',
-        label: 'SETX ' + data[1] + ' ' + newValue + ' ' + value[1],
-        subtext: value[3],
+        label: 'SETX ' + data[1] + ' ' + newValue + ' ' + variable.unit,
+        subtext: variable.note,
         execute: () => {
-            let value = values[data[1]];
-            this.$api.variables.set(value[0], value[1], newValue * value[2]);
+            let variable = variables[data[1]];
+            this.$api.variables.set(variable.name, variable.unit, newValue * variable.factor);
             return true;
         }
     };
